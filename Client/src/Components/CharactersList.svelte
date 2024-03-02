0b9db8a4373a760318ab52dc3fd1d9132910ecf3
calebtgraves <calebtgraves@gmail.com>
2024-03-01 23:10:35 -0700

    Initial commit

---
+++ Client/src/Components/CharactersList.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { allCharacters } from "../stores.mjs";
+    import CharacterIcon from "./CharacterIcon.svelte";
+    function getCharacters(){
+        const storedCharacters = localStorage.getItem("characters")
+        if(storedCharacters){
+            allCharacters.set(JSON.parse(storedCharacters))
+        }
+    }
+    getCharacters()
+</script>
+<h1>Character Select:</h1>
+<section>
+{#each $allCharacters as character}
+<CharacterIcon characterInfo={character}/>
+{/each}
+<CharacterIcon characterInfo={{
+    name:"New Character",
+    image:"src/images/new.jpg",
+    type:"new"
+}}/>
+</section>
+<style>
+    h1{
+        text-align: center;
+        margin:1rem;
+    }
+    section{
+        display:flex;
+        flex-wrap:wrap;
+        justify-content:center;
+        align-items: center;
+        height:100%;
+    }
+</style>
